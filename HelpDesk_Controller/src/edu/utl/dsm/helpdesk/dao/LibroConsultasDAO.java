b4950ca5853ee2f3e6ae461fe3b6443dea7bf83f
jessiguillen <jessiguillen106@gmail.com>
2022-12-03 19:12:53 -0600

    Actualice Libro

--- HelpDesk_Controller/src/edu/utl/dsm/helpdesk/dao/LibroConsultasDAO.java
+++ HelpDesk_Controller/src/edu/utl/dsm/helpdesk/dao/LibroConsultasDAO.java
@@ -2,6 +2,7 @@ package edu.utl.dsm.helpdesk.dao;
 
 import edu.utl.dsm.helpdesk.db.ConexionMySQL;
 import edu.utl.dsm.helpdesk.model.Libro;
+import edu.utl.dsm.helpdesk.model.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,7 +12,7 @@ import java.util.List;
 public class LibroConsultasDAO {
      public List<Libro> mostrarLibros() throws Exception {
         //Se define la consulta a ejecutar
-        String query = "select * from libro;";
+        String query = "select * from libro inner join usuario on libro.idUsuario = usuario.id";
         List<Libro> libros = new ArrayList<>();
         //Generamos el objeto de la conexion
         ConexionMySQL connMySQL = new ConexionMySQL();
@@ -23,11 +24,20 @@ public class LibroConsultasDAO {
         ResultSet rs = pstmt.executeQuery();
 
         while (rs.next()) {
-            Libro libro = new Libro();
-            libro.setId(rs.getInt("id"));
+           Libro libro = new Libro();
+            libro.setId(rs.getInt("libro.id"));
             libro.setNombre(rs.getString("nombre"));
             libro.setDescripcion(rs.getString("descripcion"));
             libro.setTema(rs.getString("tema"));
+            Usuario usuario = new Usuario();
+            usuario.setId(rs.getInt("usuario.id"));
+            usuario.setNombres(rs.getString("nombres"));
+            usuario.setApellidos(rs.getString("apellidos"));
+            usuario.setNombreUsuario(rs.getString("nombreUsuario"));
+            usuario.setEstatus(rs.getInt("estatus"));
+            usuario.setContrasennia(rs.getString("contrasennia"));
+            usuario.setRol(rs.getInt("rol"));
+            libro.setUsuario(usuario);
             libros.add(libro);
         }
         //Cerramos las conexiones
@@ -37,5 +47,41 @@ public class LibroConsultasDAO {
 
         return libros;
 
+    }
+    public List<Libro> consultarLibros(String filtro) throws Exception{
+            String query = "SELECT * FROM libro inner join usuario on libro.idUsuario = usuario.id where nombre like '"+filtro+"' or descripcion like '"
+                    +filtro+"' or tema like '"+filtro+"';";
+            List<Libro> libros = new ArrayList<>();
+        //Generamos el objeto de la conexion
+        ConexionMySQL connMySQL = new ConexionMySQL();
+
+        Connection conn = connMySQL.open();
+
+        PreparedStatement pstmt = conn.prepareStatement(query);
+
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            Libro libro = new Libro();
+            libro.setId(rs.getInt("libro.id"));
+            libro.setNombre(rs.getString("nombre"));
+            libro.setDescripcion(rs.getString("descripcion"));
+            libro.setTema(rs.getString("tema"));
+            Usuario usuario = new Usuario();
+            usuario.setId(rs.getInt("usuario.id"));
+            usuario.setNombres(rs.getString("nombres"));
+            usuario.setApellidos(rs.getString("apellidos"));
+            usuario.setNombreUsuario(rs.getString("nombreUsuario"));
+            usuario.setEstatus(rs.getInt("estatus"));
+            usuario.setContrasennia(rs.getString("contrasennia"));
+            usuario.setRol(rs.getInt("rol"));
+            libro.setUsuario(usuario);
+            libros.add(libro);
+        }
+        //Cerramos las conexiones
+        rs.close();
+        pstmt.close();
+        connMySQL.close();
+        return libros;
     }
 }
